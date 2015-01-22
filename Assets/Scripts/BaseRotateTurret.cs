af3fc336b96780989162a0384d5356d930be5ce3
MyCleaningSupplies <abdelouzzine@gmail.com>
2015-01-22 15:07:26 +0100

    uploading everything
    Tank game created in Unity.

---
+++ Assets/Scripts/BaseRotateTurret.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseRotateTurret : MonoBehaviour {
+	private Transform[] transforms;
+	protected Transform Turret;
+	protected Transform nozzle;
+	protected Vector3 targetPos;
+	// Use this for initialization
+
+
+	virtual protected void Start () {
+
+		bool turretFound = false;
+		transforms = gameObject.GetComponentsInChildren<Transform>();
+		foreach(Transform t in transforms)
+		{
+			print(t.name);
+			if (t.gameObject.name == "Turret") 
+			{
+				
+				Turret = t;
+				turretFound = true;
+			}
+			if(t.gameObject.name ==  "nozzle")
+			{
+				nozzle = t;
+			}
+		}
+		if (!turretFound) {
+			print ("no turret was found in the gameobject");
+		}
+	
+	}
+	
+	// Update is called once per frame
+	 protected virtual void Update () {
+
+		Turret.LookAt(targetPos);
+	
+	}
+}
---
+++ Assets/Scripts/GuiText.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+
+function GuiStart() {
+   if(Time.time >= 20) // If more than 20 seconds has elapsed  
+     GUI.TextArea(new Rect(10,10,100,100), "20 seconds has elapsed");
+ }
+
+
+
